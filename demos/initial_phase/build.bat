da1e4fa626730abfbb71d828e2d53708bedc4977
Luay Wadie <41401210+luaywadie@users.noreply.github.com>
2019-12-26 00:22:23 -0500

    Completed initial demo, batch script file needs updating.

--- demos/initial_phase/build.bat
+++ demos/initial_phase/build.bat
@@ -1,1 +1,3 @@
 :: Executes the procedure to compile a .g4 file and executes (runs) the results
+:: Compile G4 => java -jar C:\Users\Luay\Desktop\Research_Project\resources\antlr-4.7.2-complete.jar Hello.g4
+:: Compile Java => javac -classpath C:\Users\Luay\Desktop\Research_Project\resources\antlr-4.7.2-complete.jar Hello*.java
