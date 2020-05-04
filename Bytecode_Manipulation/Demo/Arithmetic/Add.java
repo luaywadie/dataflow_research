51a11175e47d062a1fb93e13f99cceba3d7e9909
Luay Wadie <41401210+luaywadie@users.noreply.github.com>
2020-05-04 19:04:51 -0400

    Bytecode Manipulation via javap
    Added basic samples of a java command javap that disassembles a class file to show internal code such as its Bytecode.

---
+++ Bytecode_Manipulation/Demo/Arithmetic/Add.java
@@ -0,0 +1,13 @@
+public class Add {
+  public static void main(String[] args) {
+    int input1 = 2;
+    int input2 = 3;
+    int result = add(input1,input2);
+
+    System.out.println("2 + 3 = " + result);
+  }
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
