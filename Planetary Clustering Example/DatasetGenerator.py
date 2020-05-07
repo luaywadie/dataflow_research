9350dac25ce29a150af188de761a2af744bd5f2c
Blake <ama542blake@gmail.com>
2020-05-07 09:24:47 -0400

    Add Python script/dataset for creating example with K-Means clustering
    We can regenerate the data set with more examples later, but I figured it may be easier to debug with a consistent data set between all of us.

---
+++ Planetary Clustering Example/DatasetGenerator.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+# separated out pctg features because get_commposition pctgs is easier this way
+pctg_feature_names = ['pctg_oxygen', 'pctg_helium', 'pctg_iron', 'pctg_nickel', 'pctg_silicon', 'pctg_aluminum',\
+                      'pctg_calcium', 'pctg_sodium', 'pctg_potassium', 'pctg_magnesium', 'pctg_other']
+feature_names = ['mass', 'diameter', 'has_water', 'orbits_star', 'orbits_planet', 'surface_temperature'] + pctg_feature_names
+NUM_SAMPLES = 500  # how many samples should be generated
+
+df = pd.DataFrame(columns=feature_names)
+# make boolean variables boolean
+df['has_water'] = df['has_water'].astype('boolean')
+df['orbits_star'] = df['orbits_star'].astype('boolean')
+df['orbits_planet'] = df['orbits_planet'].astype('boolean')
+
+
+# returns a normalized list of floats to add up to 1.0 for composition percentages
+def get_composition_pctgs():
+    values = np.absolute(np.random.randn(len(pctg_feature_names)))  # wrap in np.absolute so we have no negative values
+    return values / np.sum(values)
+
+
+MIN_MASS, MAX_MASS = 1000000., 1000000000000.  # 1 million, 1 trillion
+MIN_DIAMETER, MAX_DIAMETER = 1000., 1000000000.  # 1 thousand, 1 billion
+MIN_TEMP, MAX_TEMP = -10000, 10000
+
+data = []
+# generates the samples and appends them to the dataframe
+for i in range(NUM_SAMPLES):
+    mass = np.random.randint(MIN_MASS, MAX_MASS)
+    diameter = np.random.randint(MIN_DIAMETER, MAX_DIAMETER)
+    surface_temperature = np.random.randint(MIN_TEMP, MAX_TEMP)
+    has_water = bool(np.random.choice([0, 1]))
+    orbits_star = bool(np.random.choice([0, 1]))
+    orbits_planet = bool(np.random.choice([0, 1]))
+    pctg_oxygen, pctg_helium, pctg_iron, pctg_nickel, pctg_silicon, pctg_aluminum, pctg_calcium,\
+        pctg_sodium, pctg_potassium, pctg_magnesium, pctg_other = get_composition_pctgs()
+    data.append([mass, diameter, surface_temperature, has_water, orbits_star, orbits_planet,\
+                    pctg_oxygen, pctg_helium, pctg_iron, pctg_nickel, pctg_silicon, pctg_aluminum, pctg_calcium,\
+                    pctg_sodium, pctg_potassium, pctg_magnesium, pctg_other])
+
+
+# pandas docs say its quicker to build a list then append that rather than iteratively append to the df
+df2 = pd.DataFrame(data, columns=feature_names)
+df = df.append(df2)
+df.to_csv('./planetary_data.csv')
