16cba3019153808b38694cb9ce4b71e8cb0b98bf
Luay Wadie <41401210+luaywadie@users.noreply.github.com>
2020-05-08 19:54:33 -0400

    Re-created Planet Cluster Tester

---
+++ DATAVIEW/src/PlanetClusterTest.java
@@ -0,0 +1,26 @@
+import dataview.models.GlobalSchedule;
+import dataview.planners.WorkflowPlanner;
+import dataview.planners.WorkflowPlanner_Naive2;
+import dataview.workflowexecutors.WorkflowExecutor;
+import dataview.workflowexecutors.WorkflowExecutor_Local;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PlanetClusterTest {
+    public static void main(String[] args) throws IOException {
+        WorkflowVisualization frame = new WorkflowVisualization();
+        PlanetaryClustering w = new PlanetaryClustering();
+        w.design();
+        frame.drawWorkflowGraph(w);
+        WorkflowPlanner wp = new WorkflowPlanner_Naive2(w);
+        GlobalSchedule gsch = wp.plan();
+        System.out.println(gsch.getSpecification());
+        String fileLocation = System.getProperty("user.dir") + File.separator + "WebContent" +File.separator;
+        try {
+            WorkflowExecutor we = new WorkflowExecutor_Local(fileLocation+"workflowTaskDir"+ File.separator, fileLocation + "workflowLibDir"+ File.separator , gsch);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
