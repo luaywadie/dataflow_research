e507611e9e9cdc2b44d616c0b1c77612b5f2e703
Luay Wadie <41401210+luaywadie@users.noreply.github.com>
2020-05-09 17:42:38 -0400

    Updated PlanetClusterTraining

--- DATAVIEW/src/PlanetClusterTraining.java
+++ DATAVIEW/src/PlanetClusterTraining.java
@@ -18,7 +18,7 @@ public class PlanetClusterTraining extends Task {
     ins = new InputPort[1];
     outs = new OutputPort[1];
     // Assign ports
-    ins[0] = new InputPort("data", Port.DATAVIEW_Table, "This is our data input");
+    // ins[0] = new InputPort("data", Port.DATAVIEW_Table, "This is our data input");
     outs[0] = new OutputPort("centroids", Port.DATAVIEW_Table, "This is the return value from SimpleKMeans Clustering");
   }
 
