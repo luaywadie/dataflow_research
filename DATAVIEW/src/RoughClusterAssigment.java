115d32c76c26ee5368ca59c79787e0354293e7f8
Blake <ama542blake@gmail.com>
2020-06-18 10:34:23 -0400

    Updates to RoughClusterAssigment
    - Added the conversion of the clustering results to a JSON string for both upper/lower bounds sets of all clusters
    - Updated the the prt types of the output ports to be DATAVIEW_String, since we are writing JSON
    - Actually write the output now

--- DATAVIEW/src/RoughClusterAssigment.java
+++ DATAVIEW/src/RoughClusterAssigment.java
@@ -18,8 +18,8 @@ public class RoughClusterAssigment extends Task {
         // output the upper and lower bound sets
         // map from cluster no. -> list of indices (relative to the entire data set, not partition) of points in the cluster
         outs = new OutputPort[2];
-        outs[0] = new OutputPort("Lower Bound Set", Port.DATAVIEW_HashMap, "Set of cluster no. -> [cluster members]");
-        outs[1] = new OutputPort("Upper Bound Set", Port.DATAVIEW_HashMap, "Set of cluster no. -> [cluster members]");
+        outs[0] = new OutputPort("Lower Bound Set", Port.DATAVIEW_String, "JSON of cluster assignments");
+        outs[1] = new OutputPort("Upper Bound Set", Port.DATAVIEW_String, "JSON of cluster assignments");
     }
 
     @Override
@@ -32,15 +32,15 @@ public class RoughClusterAssigment extends Task {
         DATAVIEW_MathMatrix centroids = (DATAVIEW_MathMatrix) ins[1].read();
 
         // set up the upper/lower bounds sets
-        HashMap<Integer, ArrayList<Integer>> lowerBounds = new HashMap<>(RoughKMeansClustering.K);
-        HashMap<Integer, ArrayList<Integer>> upperBounds = new HashMap<>(RoughKMeansClustering.K);
+        HashMap<Integer, ArrayList<DATAVIEW_MathVector>> lowerBounds = new HashMap<>(RoughKMeansClustering.K);
+        HashMap<Integer, ArrayList<DATAVIEW_MathVector>> upperBounds = new HashMap<>(RoughKMeansClustering.K);
         for (int k = 0; k < RoughKMeansClustering.K; k++) {
             lowerBounds.put(k, new ArrayList<>());
             upperBounds.put(k, new ArrayList<>());
         }
 
         for (int i = 0; i < partition.getNumOfRows(); i++) { // go through all points in the cluster
-            int rowId = (int) rowIDs[i];
+            int rowId = (int) rowIDs.get(i);  // append to the current row AFTER we are done doing calculations with it
             DATAVIEW_MathVector row = partition.getRow(i);  // the data point we are considering
             // distance from the point to each centroid (clustDists[k] = distance to cluster k; 0 <= k < K)
             double[] clustDists = new double[centroids.getNumOfRows()];
@@ -49,34 +49,72 @@ public class RoughClusterAssigment extends Task {
             int minCluster = -1; // 0 <= minCluster < K
             double minDistance = Double.POSITIVE_INFINITY;
             for (int c = 0; c < centroids.getNumOfRows(); c++) {
-                clustDists[c] = euclideanDistance(row, centroids.getRow(c))
+                clustDists[c] = euclideanDistance(row, centroids.getRow(c));
                 if (clustDists[c] < minDistance) {
                     minDistance = clustDists[c];
                     minCluster = c;
                 }
             }
 
-            // check to see if the data point is in upper bound of any clusters
-            ArrayList<Integer> membershipSet = new ArrayList<>();
+            // check to see if the data point is in upper bound of any clusters; if so, add the data point the upper bound
+            ArrayList<DATAVIEW_MathVector> membershipSet = new ArrayList<>();  // corresponds to set T in the paper
             for (int c = 0; c < centroids.getNumOfRows(); c++) {
                 if (c != minCluster) {
-                    if (minDistance - euclideanDistance(row, centroids.getRow(c)) <= RoughKMeansClustering.THRESHOLD) {
-                        membershipSet.add(c);
+                    if (minDistance - clustDists[c] <= RoughKMeansClustering.THRESHOLD) {
+                        membershipSet.get(c).add(row.insert(0, rowId));
                     }
                 }
             }
 
             if (!membershipSet.isEmpty()) {
                 // the the row is in the upper bounds of all clusters in the membershipSet
-                for (int cluster : membershipSet) {
-                    upperBounds.get(cluster).add(rowId);
+                for (int c = 0; c < membershipSet.size(); c++) {
+                    // add the data point to the upper bound set for cluster c, with ID label attached
+                    upperBounds.get(c).add(row.insert(0, rowId));
                 }
             } else {
-                lowerBounds.get(minCluster).add(rowId);
+                // add the point with ID label attached to ONLY the LOWER bound set
+                lowerBounds.get(minCluster).add(row.insert(0, rowId));
             }
             // point is added to upperBounds of minCluster regardless of whether it is in the lower bound set
-            upperBounds.get(minCluster).add(rowId);
+            upperBounds.get(minCluster).add(row.insert(0, rowId));
         }
+
+        // TODO (refactor): this JSON stuff should probably be its own method
+        // get JSON for upper/lower bounds set for each cluster
+        JSONObject lowerBoundsJSON = new JSONObject(); // cluster no. -> [data points assigned to lower bound of cluster]
+        JSONObject upperBoundsJSON = new JSONObject(); // cluster no. -> [data points assigned to upper bound of cluster]
+        // turn ArrayList of points in each clutser to JSONArray
+        for (int k = 0; k < RoughKMeansClustering.K; k++) {  // each cluster (lower/upper bounds)
+            // LOWER BOUND
+            JSONObject lowerBoundMembers = new JSONObject();
+            for (int i = 0; i < lowerBounds.get(k).size(); i++) {  // each point assigned to the cluster
+                DATAVIEW_MathVector nextRow = lowerBounds.get(k).get(i);
+                String rowId = Double.toString(nextRow.get(0));
+                JSONArray dataPoint = new JSONArray();
+                for (int col = 0; col <= nextRow.length(); col++) {  // each feature of the point INCLUDING row ID
+                    dataPoint.add(new JSONValue(Double.toString(nextRow.get(col))));
+                }
+                lowerBoundMembers.put(rowId, new JSONValue(dataPoint));
+            }
+            lowerBoundsJSON.put(Integer.toString(k + 1), new JSONValue(lowerBoundMembers));
+
+            // UPPER BOUND
+            JSONObject upperBoundMembers = new JSONObject();
+            for (int i = 0; i < upperBounds.get(k).size(); i++) {  // each point assigned to the cluster
+                DATAVIEW_MathVector nextRow = upperBounds.get(k).get(i);
+                String rowId = Double.toString(nextRow.get(0));
+                JSONArray dataPoint = new JSONArray();
+                for (int col = 0; col <= nextRow.length(); col++) {  // each feature of the point INCLUDING row ID
+                    dataPoint.add(new JSONValue(Double.toString(nextRow.get(col))));
+                }
+                upperBoundMembers.put(rowId, new JSONValue(dataPoint));
+            }
+            upperBoundsJSON.put(Integer.toString(k + 1), new JSONValue(lowerBoundMembers));
+        }
+
+        outs[0].write(lowerBoundsJSON);
+        outs[1].write(lowerBoundsJSON);
     }
 
     private double euclideanDistance(DATAVIEW_MathVector x, DATAVIEW_MathVector y) {
--- DATAVIEW/src/RoughKMeansClustering.java
+++ DATAVIEW/src/RoughKMeansClustering.java
@@ -1,6 +1,11 @@
 import dataview.models.Workflow;
 
 public class RoughKMeansClustering extends Workflow {
-    public static final int K = 3;
+    public static final int K = 3;  // number of clusters
+    public static final int F = 7;  // number of features
     public static final double THRESHOLD = 0.1;  // TODO: how to determine good value for this?
+    public RoughKMeansClustering() {
+        // TODO: the proper constructor
+        super("fasd", "fad");
+    }
 }
